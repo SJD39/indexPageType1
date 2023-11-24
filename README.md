0d00f00fc7cbd4a3251d3a18b6bb938f3b84911a
SJD39 <sanjiudao@qq.com>
2023-11-24 09:42:39 +0800

    请不要使用这次提交，只用于记录开发过程

--- README.md
+++
@@ -1,2 +0,0 @@
-# indexPageType1
-展示网址https://sjd39.github.io/indexPageType1/
---
+++ background/bg.js
@@ -0,0 +1,15 @@
+function createBgSquare(){
+
+}
+
+function createBgTriangle(){
+
+}
+
+function createBgX(){
+
+}
+
+function createBgRound(){
+
+}
