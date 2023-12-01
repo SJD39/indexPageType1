961e36e75b1c83fe63e1a1a6e2bb2b028fbf8b7e
SJD39 <sanjiudao@qq.com>
2023-12-01 22:46:33 +0800

    开发过程记录

--- guide.js
+++ guide.js
@@ -1,22 +1,21 @@
 var loading = document.getElementsByClassName('loading')[0];
 var html = document.getElementsByTagName('html')[0];
 var body = document.getElementsByClassName('body')[0];
-var topContent = document.getElementsByClassName('topContent')[0];
-var topContentLis = topContent.getElementsByTagName('li');
+var topContentLis = firstContent.getElementsByTagName('li');
 
 window.onload = function () {
     // 标题出现动画
-    topContent.style.animation = 'appearTitle .8s';
-    topContent.style.animationFillMode = 'forwards';
+    firstContent.style.animation = 'appearTitle .8s';
+    firstContent.style.animationFillMode = 'forwards';
 
     setTimeout(function () {
         // 标题出现属性（避免最后一帧属性被后续动画重置）
-        topContent.style.opacity = "1";
-        topContent.style.top = "0px";
+        firstContent.style.opacity = "1";
+        firstContent.style.top = "0px";
 
         // 首页内容展开动画
-        topContent.style.animation = 'topContentOpen 1s';
-        topContent.style.animationFillMode = 'forwards';
+        firstContent.style.animation = 'topContentOpen 1s';
+        firstContent.style.animationFillMode = 'forwards';
 
         // 首页内容li滑入动画
         for (let i = 0; i < topContentLis.length; i = i + 2) {
