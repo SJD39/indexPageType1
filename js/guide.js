1bd67f734e52b36b25e5b3dfadbc370cac50128c
SJD39 <sanjiudao@qq.com>
2023-12-02 20:18:27 +0800

    整理代码

---
+++ js/guide.js
@@ -0,0 +1,53 @@
+var html = document.getElementsByTagName('html')[0];
+var body = document.getElementsByClassName('body')[0];
+var topContent = document.getElementsByClassName('topContent')[0];
+var topContentLis = topContent.getElementsByTagName('li');
+
+window.onload = function () {
+    loading.style.display = 'none';
+    // 标题出现动画
+    topContent.style.animation = 'appearTitle .8s';
+    topContent.style.animationFillMode = 'forwards';
+
+    setTimeout(function () {
+        // 标题出现属性（避免最后一帧属性被后续动画重置）
+        topContent.style.opacity = "1";
+        topContent.style.top = "0px";
+
+        // 首页内容展开动画
+        topContent.style.animation = 'topContentOpen 1s';
+        topContent.style.animationFillMode = 'forwards';
+
+        // 首页内容li滑入动画
+        for (let i = 0; i < topContentLis.length; i = i + 2) {
+            topContentLis[i].style.animation = 'topContentLi 1s';
+            topContentLis[i].style.animationFillMode = 'forwards';
+            topContentLis[i].style.animationDelay = i * 0.1 + 's';
+
+            topContentLis[i + 1].style.animation = 'topContentLi 1s';
+            topContentLis[i + 1].style.animationFillMode = 'forwards';
+            topContentLis[i + 1].style.animationDelay = i * 0.1 + 's';
+        }
+    }, 800);
+}
+
+// 页面滚动
+addEventListener('wheel', (event) => {
+    let speed = 800;
+
+    if (wheelLock == true) {
+        return;
+    }
+    if (event.deltaY > 0) {
+        pageDown(speed);
+    } else if (event.deltaY < 0) {
+        pageUp(speed)
+    }
+});
+
+window.onclick = function(e){
+    if(e.target.dataset.href == undefined){
+        return;
+    }
+    window.open(e.target.dataset.href)
+}
