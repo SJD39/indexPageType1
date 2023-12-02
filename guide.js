ccf55e2f2c8268be9fb675dc09a5676b86f5e6e8
SJD39 <sanjiudao@qq.com>
2023-12-02 18:18:17 +0800

    Revert "开发过程记录"
    This reverts commit 961e36e75b1c83fe63e1a1a6e2bb2b028fbf8b7e.

--- guide.js
+++ guide.js
@@ -1,21 +1,22 @@
 var loading = document.getElementsByClassName('loading')[0];
 var html = document.getElementsByTagName('html')[0];
 var body = document.getElementsByClassName('body')[0];
-var topContentLis = firstContent.getElementsByTagName('li');
+var topContent = document.getElementsByClassName('topContent')[0];
+var topContentLis = topContent.getElementsByTagName('li');
 
 window.onload = function () {
     // 标题出现动画
-    firstContent.style.animation = 'appearTitle .8s';
-    firstContent.style.animationFillMode = 'forwards';
+    topContent.style.animation = 'appearTitle .8s';
+    topContent.style.animationFillMode = 'forwards';
 
     setTimeout(function () {
         // 标题出现属性（避免最后一帧属性被后续动画重置）
-        firstContent.style.opacity = "1";
-        firstContent.style.top = "0px";
+        topContent.style.opacity = "1";
+        topContent.style.top = "0px";
 
         // 首页内容展开动画
-        firstContent.style.animation = 'topContentOpen 1s';
-        firstContent.style.animationFillMode = 'forwards';
+        topContent.style.animation = 'topContentOpen 1s';
+        topContent.style.animationFillMode = 'forwards';
 
         // 首页内容li滑入动画
         for (let i = 0; i < topContentLis.length; i = i + 2) {
