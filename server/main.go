470abb20e1c6796c129f0ad4e34786ea1151b87d
SJD39 <sanjiudao@qq.com>
2023-12-05 19:28:20 +0800

    服务器开发

--- server/main.go
+++ server/main.go
@@ -6,10 +6,18 @@ import (
 )
 
 func manage(w http.ResponseWriter, r *http.Request) {
-
+	// 后台管理
+	// 检查cookie
+	credential, err := r.Cookie("credential")
+	if err != nil {
+		fmt.Fprintln(w, "Cookie 不存在")
+		return
+	}
+	fmt.Printf(credential.Value)
 }
 
 func guide(w http.ResponseWriter, r *http.Request) {
+	// 引导页
 	fmt.Printf(r.URL.Query().Get("type"))
 }
 
