cfd069fd774a05de3c10d78766f32879c8002ae8
SJD39 <sanjiudao@qq.com>
2023-12-12 21:09:45 +0800

    开发过程

--- server/main.go
+++ server/main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func manage(w http.ResponseWriter, r *http.Request) {
-	// 后台管理
-	// 检查cookie
-	credential, err := r.Cookie("credential")
-	if err != nil {
-		fmt.Fprintln(w, "Cookie 不存在")
-		return
-	}
-	fmt.Printf(credential.Value)
+type GuideData struct {
+	Type string
+	Name string
+	Url  string
 }
 
 func guide(w http.ResponseWriter, r *http.Request) {
 	// 引导页
-	fmt.Printf(r.URL.Query().Get("type"))
+	fmt.Printf(r.URL.Query().Get("id"))
+
+	var guideData GuideData
+	guideData.Type = "external"
+	guideData.Name = "baidu"
+	guideData.Url = "https://baidu.com"
+	backJson, err := json.Marshal(guideData)
+	if err != nil {
+		log.Fatalf("Error:", err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(backJson)
 }
 
 func main() {
 	http.HandleFunc("/guide", guide)
-	http.HandleFunc("/manage", manage)
 	http.ListenAndServe(":3900", nil)
 }
