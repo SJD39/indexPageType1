f05f288a8c554b108acd06ca7394f73a3d0da773
SJD39 <sanjiudao@qq.com>
2023-12-20 13:07:12 +0800

    返回json添加id字段

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 type GuideData struct {
+	Id   uint8
 	Type string
 	Name string
 	Url  string
@@ -14,13 +15,20 @@ type GuideData struct {
 
 func guide(w http.ResponseWriter, r *http.Request) {
 	var guideData [6]GuideData
+	guideData[0].Id = 0
 	guideData[0].Type = "external"
 	guideData[0].Name = "baidu"
 	guideData[0].Url = "https://baidu.com"
 
+	guideData[1].Id = 1
 	guideData[1].Type = "here"
 	guideData[1].Name = "反应速度测试"
 	guideData[1].Url = "https://reaction.cysm.icu/"
+
+	guideData[2].Id = 2
+	guideData[3].Id = 3
+	guideData[4].Id = 4
+	guideData[5].Id = 5
 
 	backJson, err := json.Marshal(guideData)
 	if err != nil {
